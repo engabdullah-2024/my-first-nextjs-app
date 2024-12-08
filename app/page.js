e6aed6814908ebb9522b8f1b20ada37b9d7aa08d
engabdullah-2024 <enga95311@gmail.com>
2024-12-08 21:13:04 +0300

    This is my first nextjs app

--- app/page.js
+++ app/page.js
@@ -1,101 +1,45 @@
-import Image from "next/image";
+import { FaFacebookF } from "react-icons/fa6";
+export default  function Home(){
+  return(
+    <div>
+      {/* Header */}
+    
+    <div className="flex justify-between px-20 py-2 w-full bg-red-500 text-white bg-opacity-60 backdrop-blur-md shadow z-10  items-center hover:bg-white  hover:text-red-500" >
+      
+      <h1 className="text-3xl font-bold">Eng Abdalla</h1>
+      <ul className="flex gap-6">
+        <li > Home </li>
+        <li> About </li>
+        <li> Contact </li>
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </ul>
+      
     </div>
-  );
+    {/* Hero */}
+    <div className="flex justify-between px-20 py-2 mt-10">
+
+ 
+    <div>
+    <h1 className="text-5xl font-bold pb-4 border-b-4 border-red-500 inline-block mx-auto relative after:content-[''] after:block after:w-16 after:h-1:mt-2">
+  About Me
+</h1>
+
+      <p className="mt-5 text-2xl font-normal">
+      Open the my-nextjs-app folder in your favorite code editor (e.g., VS Code).
+      Start building your app by editing the files in the pages or src/app directory, depending on your configuration.
+      </p>
+      <button className="p-1 mt-5 bg-red-500 text-white text-3xl hover:bg-white hover:text-red-500 rounded-[12px]">Resume</button>
+    </div>
+    <img   className="w-[300px] h-[300px] rounded-[12px] pb-4 border-b-8 border-red-500  "  src="https://img.freepik.com/free-photo/waist-up-portrait-handsome-serious-unshaven-male-keeps-hands-together-dressed-dark-blue-shirt-has-talk-with-interlocutor-stands-against-white-wall-self-confident-man-freelancer_273609-16320.jpg?t=st=1733680181~exp=1733683781~hmac=3b4852f3a5313a66fee77ae7e4b597659d250869b4c17327e78d18a79dd94091&w=740"></img>
+    </div>
+    {/* Footer */}
+   <div className="px-20 py-2 w-full mt-20 h-[100px] text-center">
+   <h1>
+      &copy; 2024 Eng Abdalla. All rights reserved
+    
+    </h1>
+ 
+   </div>
+   </div>
+  )
 }
